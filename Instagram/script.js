109220a4ffb88090346ac6e72ba2b89076c971a3
Arman <armanansari0126@gmail.com>
2025-11-23 22:37:02 +0530

    add instagram folder

---
+++ Instagram/script.js
@@ -0,0 +1,19 @@
+var img = document.querySelector('img')
+var heart = document.querySelector("#love")
+
+img.addEventListener("dblclick",function(){
+    
+    heart.style.opacity = '1'
+    heart.style.transform = 'translate(-50%,-50%) scale(1) rotate(0deg)'
+
+     setTimeout(function () {
+        heart.style.transform = 'translate(-50%,-300%) scale(1) rotate(60deg)'
+    }, 800)
+    setTimeout(function () {
+        love.style.transform = 'translate(-50%,-50%) scale(0) rotate(-60deg)'
+    }, 1200)
+    setTimeout(function () {
+        heart.style.transform = 'translate(-50%,-50%) scale(0) rotate(-60deg)'
+    }, 1200)
+    
+})
