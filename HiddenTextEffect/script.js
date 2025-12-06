6bbb0eebf8ee449f6378848b827ff20c8ab14916
Arman <armanansari0126@gmail.com>
2025-12-06 20:54:00 +0530

    hidden text effect

---
+++ HiddenTextEffect/script.js
@@ -0,0 +1,21 @@
+const para  = document.querySelector("p");
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+const text = para.innerText
+
+para.addEventListener("mouseenter",()=>{
+    let iteration = 0;
+
+    setInterval(()=>{
+        const str = text.split('').map((char , index) =>{
+            if( index <iteration){
+                return char
+            }
+            return characters.split("")[Math.floor(Math.random()*53)]
+        }).join("")
+         para.innerText = str
+         iteration += 0.2;
+    },30)
+
+   
+
+})
